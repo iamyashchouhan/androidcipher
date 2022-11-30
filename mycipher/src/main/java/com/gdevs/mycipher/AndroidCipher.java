483c65464e4d75d2fba236c6076dedb9397f296c
iamyashchouhan <iamyashchouhan@gmail.com>
2022-11-30 15:24:35 +0530

    cipher added new

--- mycipher/src/main/java/com/gdevs/mycipher/AndroidCipher.java
+++ mycipher/src/main/java/com/gdevs/mycipher/AndroidCipher.java
@@ -10,17 +10,9 @@ public class AndroidCipher {
 
     private static String CIPHER_NAME = "AES/CBC/PKCS5PADDING";
     private static int CIPHER_KEY_LEN = 16; //128 bits
+    private static String key = "YMG-Developers77"; //128 bits
 
-
-    /**
-     * Decrypt data using AES Cipher (CBC) with 128 bit key
-     *
-     * @param key - key to use should be 16 bytes long (128 bits)
-     * @param data - encrypted data with iv at the end separate by :
-     * @return decrypted data string
-     */
-
-    public static String decrypt(String key, String data) {
+    public static String getCipher(String data) {
         try {
 
             if (key.length() < CIPHER_KEY_LEN) {
